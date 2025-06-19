e089d6702f4cb48fd4ba75cbc15e1379390d8467
Kaylee <65376239+KayleeWilliams@users.noreply.github.com>
2025-06-19 10:55:09 +0100

    more origins

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -5,5 +5,10 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
-  trustedOrigins: [getDashboardUrl()],
+  trustedOrigins: [
+    getDashboardUrl(),
+    `https://${process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL}`,
+    `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`,
+    `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`,
+  ],
 });
