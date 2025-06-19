bbd80dd3ca9a56c1e740e269d331f3538dcad690
Kaylee <65376239+KayleeWilliams@users.noreply.github.com>
2025-06-19 10:52:04 +0100

    add trused origin

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,7 +1,9 @@
 import { betterAuth } from "better-auth";
+import { getDashboardUrl } from "./env";
 
 export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
+  trustedOrigins: [getDashboardUrl()],
 });
